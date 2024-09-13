f9b455e3d66529298981e242251e6bc7ed84ab5e
fernandomora <carlos.mora@appgate.com>
2024-09-13 07:31:59 -0500

    first commit

---
+++ src/main/java/com/example/pocmysteryservice/PocMysteryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pocmysteryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMysteryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocMysteryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pocmysteryservice/PocMysteryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pocmysteryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMysteryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
