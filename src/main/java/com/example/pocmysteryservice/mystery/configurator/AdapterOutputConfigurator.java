19f1369d61e7f858ef5cf97369db48712ba875ed
fernandomora <carlos.mora@appgate.com>
2024-09-20 15:49:10 -0500

    :sparkles: move objectMapper configuration

--- src/main/java/com/example/pocmysteryservice/mystery/configurator/AdapterOutputConfigurator.java
+++ src/main/java/com/example/pocmysteryservice/mystery/configurator/AdapterOutputConfigurator.java
@@ -2,6 +2,8 @@ package com.example.pocmysteryservice.mystery.configurator;
 
 import com.example.pocmysteryservice.mystery.adapters.out.AccountPortMapImpl;
 import com.example.pocmysteryservice.mystery.application.Account;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,5 +20,12 @@ public class AdapterOutputConfigurator {
         Account ahorrosAccount = Account.from(UUID.randomUUID(), "AHORROS", 350000, new Date());
         mapPort.putAccount(ahorrosAccount);
         return mapPort;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+        return objectMapper;
     }
 }
